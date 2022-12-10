a5a38745beee75073b48fddc3a1596315da6b8e3
dyzn3y <kdmarshall728@gmail.com>
2022-12-10 17:50:14 -0500

    builds from state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,23 +4,43 @@ import Todo from './Todo';
 import TodoList from './TodoList'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state= {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, 
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state;
+    console.log(todos);
     return (
       <div>
         <h1>Todo App</h1>
         <ul>
-          <li>Task 1</li>
-          <li>Task 2</li>
-          <li>Task 3</li>
-
-          <form>
-            <label>Add Tasks
-              <input type="text" placeholder="tasks..." />
-              <button>Add</button>
-              <button>Clear Completed</button>
-            </label>
-          </form>
+          {
+            todos.map(todo => {
+              return(<li>{todo.name} {todo.completed ? <span>Completed</span>: <span></span>}</li>)
+            })
+          }
         </ul>
+        <form>
+          <label>Add Tasks
+            <input type="text" placeholder="tasks..." />
+            <button>Add</button>
+            <button>Clear Completed</button>
+          </label>
+        </form>
       </div>
     )
   }
