5cb9fa7b29b70a5eabd39eb299017565fe48c0a8
dyzn3y <kdmarshall728@gmail.com>
2022-12-10 17:18:28 -0500

    built layout of app

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Form from './Form';
+import Todo from './Todo';
+import TodoList from './TodoList'
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todo App</h1>
+        <ul>
+          <li>Task 1</li>
+          <li>Task 2</li>
+          <li>Task 3</li>
+
+          <form>
+            <label>Add Tasks
+              <input type="text" placeholder="tasks..." />
+              <button>Add</button>
+              <button>Clear Completed</button>
+            </label>
+          </form>
+        </ul>
       </div>
     )
   }
